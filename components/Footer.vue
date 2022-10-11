084441bc9f72f2e265768716d2d8ca0afdc49d5b
Lolu Adelore <adeloreisaac@gmail.com>
2022-10-11 15:58:44 +0100

    initial commit

---
+++ components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-dark tl_footer_BG" id="js_footer">
+    <div class="container py-5">
+      <div
+        class="row"
+      >
+        <div class="col-6 tl_footer_links_row">
+          <h1>Bets</h1>
+          <a href="/Sport">Sports</a> <br>
+          <a href="/Sport/Live">Live</a> <br>
+          <a href="/Sport/Esportview">E-Sports Bets</a> <br>
+        </div>
+        <div class="col-6 tl_footer_links_row">
+          <h1>Promotions</h1>
+          <a href="/Promotions">Welcome Bonus</a> <br>
+          <a href="/Promotions">Cashout</a> <br>
+          <a href="/Promotions">Multi Bonus</a> <br>
+        </div>
+        <div class="col-12 tl_footer_links_row">
+          <h1>Support</h1>
+          <a href="/Rules/en" target="_blank">General Terms and Conditions</a> <br>
+          <a href="/Rules/en" target="_blank">FAQ</a> <br>
+          <a href="/Rules/en" target="_blank">Contact Us</a> <br>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-sm py-3 navbar-light bg-transparent">
+      <div class="tl_logo">
+        <a href="/">
+          <img src="~/assets/img/logo.png" style="height: 50px" alt="" />
+        </a>
+      </div>
+      <!-- <button
+        class="navbar-toggler d-lg-none"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsibleNavId"
+        aria-controls="collapsibleNavId"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button> -->
+      <ul class="navbar-nav mr-4 ml-auto mt-2 mt-lg-0">
+
+          <li class="nav-item d-none d-lg-block">
+            <a class="nav-link text-white" style="font-size: 18px" href="#">Livescore</a>
+          </li>
+        </ul>
+        <div class="tl_logged_out">
+          <a
+            name=""
+            id=""
+            class="btn btn-outline-secondary text-white mr-2"
+            href="#"
+            role="button"
+            >LOGIN</a
+          >
+
+        </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.tl_logo > a > img {
+  flex-shrink: 0;
+  flex-grow: 0;
+  width: auto;
+  height: 40px;
+}
+</style>
